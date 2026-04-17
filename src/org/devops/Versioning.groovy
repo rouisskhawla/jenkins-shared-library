92da9c5e53fd86e6836049fc0c9877b6413f79f2
Khawla Rouis <rouis.khaoula@worldsoftgroup.com>
2026-04-17 17:29:28 +0100

    Jenkins native versioning

--- src/org/devops/Versioning.groovy
+++ src/org/devops/Versioning.groovy
@@ -4,40 +4,25 @@ class Versioning {
 
     static String generate(String imageName, String branch, String buildNumber, String gitCommit) {
 
-        def parts = imageName.split('/')
-        def org = parts[0]
-        def repo = parts[1]
+        def shortCommit = gitCommit?.take(7) ?: "unknown"
 
-        // Docker Hub API call
-        def url = "https://hub.docker.com/v2/repositories/${org}/${repo}/tags/?page_size=100"
+        def baseVersion = "1.0.0" 
 
-        def connection = new URL(url).openConnection()
-        connection.setRequestProperty("Accept", "application/json")
+        def (major, minor, patch) = baseVersion.tokenize('.').collect { it as int }
 
-        def response = connection.inputStream.getText("UTF-8")
-
-        def json = new groovy.json.JsonSlurper().parseText(response)
-
-        def tags = json.results.collect { it.name }
-                .findAll { it ==~ /^[0-9]+\.[0-9]+\.[0-9]+(-[a-z0-9]+)?$/ }
-
-        def baseVersion = tags ? tags.sort(false).last() : "0.0.0"
-
-        def versionNumber = baseVersion.split('-')[0]
-        def (major, minor, patch) = versionNumber.tokenize('.').collect { it as int }
-
-        patch = patch + 1
-
-        def version
+        patch = patch + buildNumber.toInteger()
 
         if (branch == "dev") {
-            version = "${major}.${minor}.${patch}-dev"
-        } else if (branch == "main") {
-            version = "${major}.${minor}.${patch}"
-        } else {
-            throw new RuntimeException("Unsupported branch: ${branch}")
+            return "${imageName}:${major}.${minor}.${patch}-dev-${shortCommit}"
         }
 
-        return version
+        if (branch == "main") {
+            return "${imageName}:${major}.${minor}.${patch}-${shortCommit}"
+        }
+        else {
+            return "${imageName}:${major}.${minor}.${patch}-${branch}-${shortCommit}"
+        }
+
+        return "${imageName}:${major}.${minor}.${patch}-${shortCommit}"
     }
 }
